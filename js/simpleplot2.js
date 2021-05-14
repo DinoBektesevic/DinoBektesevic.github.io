6ee0aecaa556d29ffe5ce2c2ac6ffaa51717c0f8
DinoBektesevic <dinob@uw.edu>
2021-05-13 22:42:50 -0700

    Interactive all fields visualization.

--- js/simpleplot2.js
+++ js/simpleplot2.js
@@ -25,123 +25,112 @@
  */
 function simplePlot2(elementid){
     // <td><svg id="growthplt" width = "500" height = "500"></svg></td>
+
+    //var data = d3.range(0, 2 * Math.PI, .01).map(function(t) {
+    //    return [t, Math.sin(2 * t) * Math.cos(2 * t)];
+    //});
+
+    var width = 960,
+        height = 500,
+        radius = Math.min(width, height) / 2 - 30;
+
+    var r = d3.scaleLinear()
+        .domain([0, 1])
+        .range([0, radius]);
+
+    var theta = d3.scaleLinear().range(0, width);
+    //    var r = d3.scaleLinear().range(0, height).domain([0, 1]);
+
+    var line = d3.lineRadial()
+        .radius(function(d) { return r(d.r); })
+        .angle(function(d) { return d.azRad; });
+
     var svg = d3.select(elementid).append("svg")
-        .attr("height", 500)
-        .attr("width", 500);
-		margin = 200;
-		width = svg.attr("width") - margin;
-		height = svg.attr("height") - margin;
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-		svg.append("text")
-        .attr("transform", "translate(100,0)")
-		    .attr("x", 50).attr("y", 50)
-		    .attr("font-size", "20px")
-		    .attr("class", "title")
-		    .text("Population bar chart");
+    var gr = svg.append("g")
+        .attr("class", "r axis")
+        .selectAll("g")
+        .data(r.ticks(5).slice(1))
+        .enter().append("g");
 
-	  x = d3.scaleBand().range([0, width]).padding(0.4);
-		y = d3.scaleLinear().range([height, 0]);
+    gr.append("circle")
+        .attr("r", r);
 
-		g1 = svg.append("g")
-        .attr("transform", "translate(" + 100 + "," + 100 + ")");
+    gr.append("text")
+        .attr("y", function(d) { return -r(d) - 4; })
+        .attr("transform", "rotate(15)")
+        .style("text-anchor", "middle")
+        .text(function(d) { return d; });
 
-		d3.csv("../data/data.csv").then(function(data) {
-		    x.domain(data.map(function(d) { return d.year; }));
-		    y.domain([0, d3.max(data, function(d) { return d.population; })]);
+    var ga = svg.append("g")
+        .attr("class", "a axis")
+        .selectAll("g")
+        .data(d3.range(0, 360, 30))
+        .enter().append("g")
+        .attr("transform", function(d) { return "rotate(" + -d + ")"; });
 
-		    g1.append("g")
-		     .attr("transform", "translate(0," + height + ")")
-		     .call(d3.axisBottom(x))
-		     .append("text")
-		     .attr("y", height - 250)
-		     .attr("x", width - 100)
-		     .attr("text-anchor", "end")
-		     .attr("font-size", "18px")
-		     .attr("stroke", "blue").text("year");
+    ga.append("line")
+        .attr("x2", radius);
 
-		    g1.append("g")
-		        .append("text")
-		        .attr("transform", "rotate(-90)")
-		        .attr("y", 6)
-		        .attr("dy", "-5.1em")
-		        .attr("text-anchor", "end")
-		        .attr("font-size", "18px")
-		        .attr("stroke", "blue")
-		        .text("population");
-
-		    g1.append("g")
-		        .attr("transform", "translate(0, 0)")
-		        .call(d3.axisLeft(y));
-
-		    g1.selectAll(".bar1")
-		        .data(data)
-		        .enter()
-		        .append("rect")
-		        .attr("class", "bar1")
-		        .on("mouseover", simplePlot2OnMouseOver)
-		        .on("mouseout", simplePlot2OnMouseOut)
-		        .attr("x", function(d) { return x(d.year); })
-		        .attr("y", function(d) { return y(d.population); })
-		        .attr("width", x.bandwidth()).transition()
-		        .ease(d3.easeLinear).duration(200)
-		        .delay(function (d, i) {
-		            return i * 25;
-		        })
-		        .attr("height", function(d) { return height - y(d.population); });
-		});
-};
+    ga.append("text")
+        .attr("x", radius + 6)
+        .attr("dy", ".35em")
+        .style("text-anchor", function(d) { return d < 270 && d > 90 ? "end" : null; })
+        .attr("transform", function(d) { return d < 270 && d > 90 ? "rotate(180 " + (radius + 6) + ",0)" : null; })
+        .text(function(d) { return d + "°"; });
 
 
-// I don't know why these functions HAVE TO be defined after we already called them
-// but there we go and here we are
-/**
- * Event registered when mouse is positioned over the bar in a simple
- * plot. The bar will change color and highlight.
- * @param  {Object} d Rectangle object selected by the cursor.
- * @param  {Number} i Zero based Index of the selected rectangle.
- */
-function simplePlot2OnMouseOver(i, d) {
-    d3.select(this)
-        .attr('class', 'highlight');
-
-    d3.select(this)
-        .transition()
-        .duration(200)
-        .attr('width', x.bandwidth() + 5)
-        .attr("y", function(d) { return y(d.population) - 10; })
-        .attr("height", function(d) { return height - y(d.population) + 10; });
-
-    g1.append("text")
-        .attr('class', 'val')
-        .attr('x', function() {
-            return x(d.year);
-        })
-        .attr('y', function() {
-            return y(d.population) - 10;
+    let select = d3.select("body")
+        .append("select")
+        .attr("id", "button")
+        .on("change", function() {
+            // Log value it is changed to:
+            // console.log(this.value);
+            draw(this.value);
         });
-}
 
+    d3.csv("../data/mjd-59418-sdss-simple-expanded-polar.csv").then(function(data) {
+        dates = new Set(data.map(function(d){ return d.mjdExpStart;}));
+        fields = new Set(data.map(function(d){ return d.mjdExpStart;}));
 
-/**
- * Event registered when mouse is positioned leves a bar on a simple
- * plot. Color and highlight are reset to original values.
- * @param  {Object} d Rectangle object selected by the cursor.
- * @param  {Number} i Zero based Index of the selected rectangle.
- */
-function simplePlot2OnMouseOut(i, d) {
+        // Add the columns as options:
+        var options = select.selectAll(null)
+            .data(dates.keys())
+            .enter()
+            .append("option")
+            .text(function(d) { return d; });
+    });
 
-    d3.select(this)
-        .attr('class', '.bar1');
+    var draw = function(fieldid) {
 
+        svg.selectAll("#point").remove();
 
+        d3.csv("../data/mjd-59418-sdss-simple-expanded-polar.csv").then(function(data) {
+            filteredData = data.filter(function(d){ return d.mjdExpStart == fieldid; });
 
-    d3.select(this)
-        .transition()
-        .duration(200)
-        .attr('width', x.bandwidth())
-        .attr("y", function(d) { return y(d.population); })
-        .attr("height", function(d) { return height - y(d.population); });
+            point = svg.selectAll("point")
+                .data(filteredData);
 
-    d3.selectAll('.val')
-        .remove();
-}
+            //point = svg.selectAll("point")
+            //    .data(filteredData);
+
+            point.enter()
+                .append("circle")
+                .attr("id", "point")
+                .attr("transform", function(d) {
+                    // use the line function and parse out the coordinates
+                    var coors = line([d]).slice(1).slice(0, -1);
+                    return "translate(" + coors + ")";
+                })
+                .attr("r", 1);
+            // svg.selectAll('circle').data(filteredData).exit().remove();
+            // svg.selectAll('circle').data(filteredData).exit().remove();
+        });
+    };
+
+    svg.exit().remove();
+};
